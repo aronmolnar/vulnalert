5f7ad1d8381ee5ece80db52996493c527820be7d
aron <aron.molnar@a1telekom.at>
2021-08-11 19:05:56 +0200

    Update naming

--- src/utils/mappings.py
+++ src/utils/mappings.py
@@ -4,8 +4,8 @@ from publish.printout import printout
 
 PUBLISH_TYPES = {
     'printout': printout,
-    'mailgun': mailgun_digest,
+    'mailgun_digest': mailgun_digest,
     'mailgun_warning': mailgun_warning,
-    'mailjet': mailjet_digest,
+    'mailjet_digest': mailjet_digest,
     'mailjet_warning': mailjet_warning,
 }
