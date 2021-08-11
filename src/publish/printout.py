fab0272a113a0c7f54e69b1fa30baf6f5fdfafc1
aron <aron.molnar@a1telekom.at>
2021-08-11 19:09:03 +0200

    Better printout if no news available

--- src/publish/printout.py
+++ src/publish/printout.py
@@ -3,4 +3,4 @@ from utils.helper import articles_to_message
 
 def printout(articles, dry_run=False):
     # dry_run is ignored. printout can always be called
-    print(articles_to_message(articles, add_footer=True))
+    print(articles_to_message(articles, add_footer=True) or 'No news available.')
