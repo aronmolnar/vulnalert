a48560c9ef32c7d8d7cfab88942e61489860e046
aron <aron.molnar@a1.digital>
2021-10-18 09:33:15 +0200

    Set datetime explicitly if badly delivered by source

--- src/source_classes/rss.py
+++ src/source_classes/rss.py
@@ -13,10 +13,13 @@ class RSS(Source):
         socket.setdefaulttimeout(5)
         parsed_feed = feedparser.parse(self.url)
         for entry in parsed_feed.get('entries', list()):
+            publish_time = datetime(*entry['published_parsed'][:6])
+            if publish_time.hour == publish_time.minute == publish_time.second == 0:
+                publish_time = datetime.now()
             article = Article(
                 title=entry['title'],
                 url=entry['id'],
-                publish_time=datetime(*entry['published_parsed'][:6]),
+                publish_time=publish_time,
                 article_type=self.main_type,
                 language=self.language,
             )
